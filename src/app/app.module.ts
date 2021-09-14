dc6ffbcfa68275069bd9eeb6209127e4ef0dcf72
DhayananthP <62584108+DhayananthP@users.noreply.github.com>
2021-09-14 10:44:07 +0530

    Add files via upload
    initial setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
